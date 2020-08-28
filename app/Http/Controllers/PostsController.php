68254f6eae00111d79fa67b64bab2ea23a1e7bfa
jguedez92 <jguedez92@gmail.com>
2020-08-28 22:48:34 +0200

    endpoint for insert posts added on controllers and routes

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -19,4 +19,23 @@ class PostsController extends Controller
             ], 500);
         }
     }
+
+    public function insertPost(Request $request){
+        try {
+            $post = $request->validate([
+                'title' => 'required|string|max:40',
+                'body' => 'required|string',
+                'userId' => 'required|numeric',
+            ]);
+            $request = Http::withHeaders([
+                "Content-type" => "application/json; charset=UTF-8"
+                ])->post('https://jsonplaceholder.typicode.com/posts', $post );
+            return response(['messaje' => 'post inserted successfully']);
+        } catch (\Exception $e) {
+            return response([
+                'message' => 'There was an error trying to get the posts',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,16 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
@@ -22,7 +12,7 @@ Route::prefix('users')->group(function () {
     Route::get('get_users','UsersController@getUsers');
 });
 
-
 Route::prefix('posts')->group(function () {
     Route::get('get_posts','PostsController@getPosts');
+    Route::post('insert_posts','PostsController@insertPost');
 });
